60145edcb493e812a3a027d730a562db2a257f1c
bydlovers <bydloverstr@gmail.com>
2025-10-04 15:07:49 +0300

    sevicebyd-destek-cache-v15

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // service-worker.js
 
-const CACHE_NAME = 'byd-destek-cache-v14'; // Sürüm V6'ya yükseltildi
-const DYNAMIC_CACHE_NAME = 'byd-destek-dynamic-v14'; 
+const CACHE_NAME = 'byd-destek-cache-v15'; // Sürüm V6'ya yükseltildi
+const DYNAMIC_CACHE_NAME = 'byd-destek-dynamic-v15'; 
 
 // Uygulamanın çevrimdışı çalışması için gerekli tüm statik dosyalar
 const urlsToCache = [

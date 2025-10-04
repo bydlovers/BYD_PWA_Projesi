6236194fed3ef949ad539a3905abe126b3e551e6
bydlovers <bydloverstr@gmail.com>
2025-10-04 11:17:51 +0300

    gemini saçmladı

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // service-worker.js
 
-const CACHE_NAME = 'byd-destek-cache-v10'; // Sürüm V6'ya yükseltildi
-const DYNAMIC_CACHE_NAME = 'byd-destek-dynamic-v10'; 
+const CACHE_NAME = 'byd-destek-cache-v12'; // Sürüm V6'ya yükseltildi
+const DYNAMIC_CACHE_NAME = 'byd-destek-dynamic-v12'; 
 
 // Uygulamanın çevrimdışı çalışması için gerekli tüm statik dosyalar
 const urlsToCache = [
